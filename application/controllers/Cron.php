925cb3e41b48c583af263bb383c28f79e9ddf76b
appsoulmate <graham@appmatch.com>
2017-05-11 16:03:37 +0530

    commit for cron file

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -8,7 +8,7 @@ class Cron extends CI_Controller {
         parent::__construct();
 
         $this->load->library('facerecognition');
-        $this->load->library('device_notification');
+//        $this->load->library('device_notification');
         $this->load->library('push_notification');
 
         $this->load->model(array('icp_images_model', 'users_model', 'icp_imagetag_model', 'settings_model', 'icps_model'));
