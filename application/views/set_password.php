df3cef6b8a6571e7c122daffa36cde86be58312d
appsoulmate <graham@appmatch.com>
2017-10-26 13:02:54 +0530

    COMMIT FOR NEW CHANGES

--- application/views/set_password.php
+++ application/views/set_password.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!-- Reset Password form -->
 <section id="register" class="sections">
     <div class="container">
@@ -67,74 +66,4 @@
             $(this).next('span').show();
         }
     });
-=======
-<!-- Reset Password form -->
-<section id="register" class="sections">
-    <div class="container">
-        <div class="row"> 
-            <!-- Heading-->
-            <div class="heading wow fadeIn animated" data-wow-offset="120" data-wow-duration="1.5s">
-                <div class="title text-center">
-                    <h1>Set Password</h1>
-                </div>
-                <div class="subtitle text-center ">
-                    <h6>Please enter your password and confirm entered password.</h6>
-                </div>
-                <div class="separator_wrap">
-                    <div class="separator2"></div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-sm-offset-3"> 
-                <?php
-                if ($this->session->flashdata('success')) {
-                    echo '<div class="alert alert-success" id="msg_success">' . $this->session->flashdata('success') . '</div>';
-                }
-                ?>
-                <?php
-                if ($this->session->flashdata('error')) {
-                    echo '<div class="alert alert-danger" id="msg_error">' . $this->session->flashdata('error') . '</div>';
-                }
-                ?>
-                <!-- LOGIN FORM -->
-                <div class="reg-wrap login-reg-wrap">
-                    <form method="post" class="login-form" id="loginform">
-                        <div class="step-block" id="firstblock">
-                            <div id="fancy-inputs">
-                                <div class="form-group">
-                                    <label class="input">
-                                        <input type="password" id="password" name="password" class="name <?php echo (form_error('password') ? 'error-input' : '') ?>">
-                                        <span><span>Password</span></span> 
-                                        <div id="err_password"><?php echo form_error('password'); ?></div>
-                                    </label>
-                                </div>
-                                <div class="form-group">
-                                    <label class="input">
-                                        <input type="password" id="con_password" name="con_password" class="name <?php echo (form_error('con_password') ? 'error-input' : '') ?>">
-                                        <span><span>Confirm Password</span></span> 
-                                        <div id="err_con_password"><?php echo form_error('con_password'); ?></div>
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="submit-button">
-                                <button class="btn btn-primary btnxs first_btn" name="first"><i class="fa fa-paper-plane"></i> &nbsp;Set Password</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<script>
-    $('#msg_success').delay(5000).fadeOut('slow');
-    $('#msg_error').delay(5000).fadeOut('slow');
-
-    $('input:password').on('change', function () {
-        if ($(this).val().length) {
-            $(this).next('span').hide();
-        } else {
-            $(this).next('span').show();
-        }
-    });
->>>>>>> 3953b9284fa430959cd25bd3f245cdbb609c1f79
 </script>
